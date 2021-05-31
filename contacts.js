56e9328af0e9c4abe679b666395af3a63ffe354a
taijayson <71400805+taijayson@users.noreply.github.com>
2021-06-01 00:31:48 +0300

    contacts.js added

--- contacts.js
+++ contacts.js
@@ -1,44 +1,79 @@
 const fs = require("fs").promises;
 const path = require("path");
 
-// console.log(path);
-
-/*
- * Раскомментируй и запиши значение
- */
-
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 // console.log(contactsPath);
 
 // TODO: задокументировать каждую функцию
+
+//===============GET CONTACTS==============//
+
 const listContacts = async (filePath) => {
   try {
     const data = await fs.readFile(filePath).then((data) => JSON.parse(data));
-    // console.log(data);
+    console.log(data);
     return data;
   } catch (err) {
     throw err;
   }
 };
 
-// console.log(listContacts(contactsPath));
+console.log(listContacts(contactsPath));
 
 const getContactById = async (contactId, filePath) => {
   try {
     const data = await listContacts(filePath);
     // console.log(data);
     const find = data.find((item) => item.id === contactId);
-    console.log(find);
+    // console.log(find);
     return find;
   } catch (err) {
     throw err;
   }
 };
 
-console.log(getContactById("3", contactsPath));
+// console.log(getContactById("3", contactsPath));
 
-const removeContact = (contactId, filePath) => {};
+//===============ADD AND REMOVE CONTACTS==============//
 
-const addContact = (name, email, phone) => {
-  // ...твой код
+const addContact = async (id, name, email, phone, filePath) => {
+  try {
+    const data = await listContacts(filePath);
+    const newItem = {
+      id: id,
+      name: name,
+      email: email,
+      phone: phone,
+    };
+    data.push(newItem);
+    fs.writeFile(filePath, JSON.stringify(data));
+    console.log(data);
+  } catch (err) {
+    throw err;
+  }
+};
+
+// console.log(
+//     addContact("4", "Shang Csung", "shang@mail.com", "0555555555", contactsPath),
+//   addContact("5", "Kung Lao", "lao@mail.com", "06666666666", contactsPath)
+// );
+
+const removeContact = async (contactId, filePath) => {
+  try {
+    const data = await listContacts(filePath);
+    const newData = data.filter(({ id }) => id !== contactId);
+    fs.writeFile(filePath, JSON.stringify(newData));
+    // console.log(data);
+  } catch (err) {
+    throw err;
+  }
+};
+
+// console.log(removeContact("5", contactsPath));
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
 };
