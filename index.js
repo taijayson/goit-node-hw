856d338ff966ef05a480352a7ec0d0c9123330e7
taijayson <71400805+taijayson@users.noreply.github.com>
2021-05-31 21:41:07 +0300

    some func added

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-const date = new Date();
-console.log(date.getMilliseconds());
+const argv = require("yargs").argv;
+// console.log(argv);
+
+// TODO: рефакторить
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      console.log("list");
+      break;
+    case "get":
+      console.log("id", id);
+      break;
+    case "add":
+      console.log("name email phone", name, email, phone);
+      break;
+    case "remove":
+      console.log("id", id);
+      break;
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+invokeAction(argv);
