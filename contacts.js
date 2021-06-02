5d37da6b8f2a1b3549e4704b04f7c11fd5409f01
taijayson <71400805+taijayson@users.noreply.github.com>
2021-06-02 19:40:29 +0300

    comments clear

--- contacts.js
+++ contacts.js
@@ -3,9 +3,6 @@ const path = require("path");
 const { v4 } = require("uuid");
 
 const contactsPath = path.join(__dirname, "db", "contacts.json");
-// console.log(contactsPath);
-
-// TODO: задокументировать каждую функцию
 
 //===============GET CONTACTS==============//
 
@@ -14,28 +11,21 @@ const listContacts = async () => {
     const data = await fs
       .readFile(contactsPath)
       .then((data) => JSON.parse(data));
-    // console.log(data);
     return data;
   } catch (err) {
     throw err;
   }
 };
 
-// listContacts();
-
 const getContactById = async (contactId) => {
   try {
     const data = await listContacts();
-    // console.log(data);
     const find = data.find((item) => item.id === contactId);
-    // console.log(find);
     return find;
   } catch (err) {
     throw err;
   }
 };
-
-// getContactById("3");
 
 //===============ADD AND REMOVE CONTACTS==============//
 
@@ -50,27 +40,20 @@ const addContact = async (id, name, email, phone) => {
     };
     data.push(newItem);
     fs.writeFile(contactsPath, JSON.stringify(data));
-    // console.log(data);
   } catch (err) {
     throw err;
   }
 };
-
-// addContact("4", "Shang Csung", "shang@mail.com", "0555555555");
-// addContact("5", "Kung Lao", "lao@mail.com", "06666666666");
 
 const removeContact = async (contactId) => {
   try {
     const data = await listContacts();
     const newData = data.filter(({ id }) => id !== contactId);
     fs.writeFile(contactsPath, JSON.stringify(newData));
-    // console.log(data);
   } catch (err) {
     throw err;
   }
 };
-
-// removeContact("4");
 
 module.exports = {
   listContacts,
